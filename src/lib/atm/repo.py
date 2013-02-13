784651a50125817c8a54ebc22bbb7979b60a9687
joamag <joamag@gmail.com>
2013-02-13 16:36:40 +0000

    initial support for the git version operation

--- src/lib/atm/repo.py
+++ src/lib/atm/repo.py
@@ -43,6 +43,8 @@ import subprocess
 import atm
 
 def git(url = None, path = None, clean = False):
+    # retrieves the various default values from the
+    # configuration in case their were not provided
     url = url or atm.conf("repo", None)
     path = path or atm.path("repo", None)
 
@@ -61,3 +63,70 @@ def git(url = None, path = None, clean = False):
     gitignore_path = os.path.join(path, ".gitignore")
     atm.remove(git_path)
     atm.remove(gitignore_path)
+
+def git_v(_version, url = None):
+    # retrieves the various default values from the
+    # configuration in case their were not provided
+    url = url or atm.conf("repo", None)
+
+    # clones the repository to the target (verify) directory
+    # to be used for the processing of the log
+    result = subprocess.call([
+        "git",
+        "clone",
+        url,
+        "verify",
+        "--quiet"
+    ])
+    if not result == 0: raise RuntimeError("Problem cloning repository using git")
+
+    # retrieves the current working directory so that it's saved
+    # and then changed the current directory to the verify directory
+    cwd = os.getcwd()
+    os.chdir("verify")
+    try:
+        # puts the current revision in the head branch into
+        # the version file and then opens the file to be able
+        # to prettify it into the normalized version value
+        result = subprocess.call([
+            "git",
+            "rev-parse",
+            "HEAD",
+            ">",
+            "VERSION"
+        ])
+        if not result == 0: raise RuntimeError("Problem executing 'rev-parse' using git")
+
+        # opens the created version file and reads the version from
+        # it by stripping all the extra characters from it
+        version_file = open("VERSION")
+        try: version = version_file.read().strip()
+        finally: version_file.close()
+
+        # calls the appropriate log command for the git according to
+        # the existence or not of the previous version value
+        if _version: result = subprocess.call([
+            "git",
+            "log",
+            _version,
+            "..HEAD",
+            "--pretty=format:\"%h%x09%an%x09%ad%x09%s\"",
+            ">",
+            "LOG"
+        ])
+        else: result = subprocess.call([
+            "git",
+            "log",
+            "--pretty=format:\"%h%x09%an%x09%ad%x09%s\"",
+            ">",
+            "LOG"
+        ])
+        if not result == 0: raise RuntimeError("Problem executing 'log' using git")
+    finally:
+        # changes the directory back to the original value in
+        # order to restore the settings to the original value
+        os.chdir(cwd)
+
+    # returns the version that was "just" read from the git
+    # repository, this value should be verified
+    return version
