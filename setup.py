0e65a7dc09a13d5fa6fb1e2b3e866c42dedd2d43
joamag <joamag@gmail.com>
2013-02-23 13:02:17 +0000

    new version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if not root_directory == "": os.chdir(root_directory)
 
 setuptools.setup(
     name = "automium",
-    version = "0.1.19",
+    version = "0.1.20",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Automium System",
