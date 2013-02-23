e94b8a21c02b686dcc9e6c664fceaf8ef18e971f
joamag <joamag@gmail.com>
2013-02-23 12:18:26 +0000

    new version value

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if not root_directory == "": os.chdir(root_directory)
 
 setuptools.setup(
     name = "automium",
-    version = "0.1.15",
+    version = "0.1.16",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Automium System",
--- src/lib/automium.py
+++ src/lib/automium.py
@@ -61,7 +61,7 @@ TIMESTAMP_PRECISION = 100.0
 """ The precision to be used for the timestamp integer
 identifier calculation (more precision less collisions) """
 
-VERSION = "0.1.15"
+VERSION = "0.1.16"
 """ The version value """
 
 RELEASE = "130"
