62648fc0268c7a8358833d628e1b54d81ff83856
joamag <joamag@gmail.com>
2013-11-14 23:13:46 +0000

    new version value

--- src/lib/automium.py
+++ src/lib/automium.py
@@ -62,7 +62,7 @@ TIMESTAMP_PRECISION = 100.0
 """ The precision to be used for the timestamp integer
 identifier calculation (more precision less collisions) """
 
-VERSION = "0.1.30"
+VERSION = "0.1.31"
 """ The current version value for the automium executable """
 
 RELEASE = "130"
